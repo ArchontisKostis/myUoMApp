e7b6a4c76ad165fcab95c4dbcda743118ce6c7ef
DESKTOP-EMSE65H\Archontis <arxontisk02@gmail.com>
2023-03-13 21:17:36 +0200

    create HomeActivity

--- app/src/main/java/com/example/myuomapp/MainActivity.java
+++ app/src/main/java/com/example/myuomapp/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.myuomapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = (Button) findViewById(R.id.login_btn);
+        loginButton.setOnClickListener(view -> {
+            Log.w("INFO", "Fake production sign in (MainActivity)");
+            Intent intent = new Intent(this, HomeActivity.class);
+            Log.i("INFO", "Start HomeActivity");
+            startActivity(intent);
+        });
+
+        Log.i("INFO", "MainActivity created");
     }
 }
