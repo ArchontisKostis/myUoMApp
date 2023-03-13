ad17bc657aa6de528a9fdfcccde7514d5744063c
DESKTOP-EMSE65H\Archontis <arxontisk02@gmail.com>
2023-03-13 21:54:44 +0200

    Add web activity functionality
    We can now view eclass and studentsweb straight from the app

--- app/src/main/java/com/example/myuomapp/HomeActivity.java
+++ app/src/main/java/com/example/myuomapp/HomeActivity.java
@@ -2,13 +2,36 @@ package com.example.myuomapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+    Button eClassBtn;
+    Button studentsWebBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        findViews();
+
+        eClassBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(this, WebActivity.class);
+            intent.putExtra("url", "https://eclass.uom.gr/");
+            startActivity(intent);
+        });
+
+        studentsWebBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(this, WebActivity.class);
+            intent.putExtra("url", "https://services.uom.gr/unistudent/login.asp");
+            startActivity(intent);
+        });
+    }
+
+    private void findViews() {
+        eClassBtn = (Button) findViewById(R.id.services_btn_1);
+        studentsWebBtn = (Button) findViewById(R.id.services_btn_2);
     }
 }
---
+++ app/src/main/java/com/example/myuomapp/WebActivity.java
@@ -0,0 +1,27 @@
+package com.example.myuomapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class WebActivity extends AppCompatActivity {
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+
+        Intent intent = getIntent();
+        String intentUrl = intent.getStringExtra("url");
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.loadUrl(intentUrl);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+}
