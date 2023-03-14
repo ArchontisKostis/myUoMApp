70d2a326b07b7d829ce414bbaf7883927c77abf8
DESKTOP-EMSE65H\Archontis <arxontisk02@gmail.com>
2023-03-14 17:31:02 +0200

    refactoring

--- app/src/main/java/com/example/myuomapp/HomeActivity.java
+++ app/src/main/java/com/example/myuomapp/HomeActivity.java
@@ -10,36 +10,45 @@ public class HomeActivity extends AppCompatActivity {
     Button eClassBtn;
     Button studentsWebBtn;
     Button eudoxusBtn;
+    Button universityWebsiteBtn;
+    Button studentCareBtn;
+    Button graduationBtn;
+    Button libraryBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        // Find UI Components
         findViews();
 
-        eClassBtn.setOnClickListener(view -> {
-            Intent intent = new Intent(this, WebActivity.class);
-            intent.putExtra("url", getString(R.string.e_classroom_url));
-            startActivity(intent);
-        });
-
-        studentsWebBtn.setOnClickListener(view -> {
-            Intent intent = new Intent(this, WebActivity.class);
-            intent.putExtra("url", getString(R.string.students_portal_url));
-            startActivity(intent);
-        });
-
-        eudoxusBtn.setOnClickListener(view -> {
-            Intent intent = new Intent(this, WebActivity.class);
-            intent.putExtra("url", getString(R.string.eudoxus_url));
-            startActivity(intent);
-        });
+        // Set listeners to the buttons
+        setButtonOnClickListener(eClassBtn, getString(R.string.e_classroom_url));
+        setButtonOnClickListener(studentsWebBtn, getString(R.string.students_portal_url));
+        setButtonOnClickListener(eudoxusBtn, getString(R.string.eudoxus_url));
+        setButtonOnClickListener(universityWebsiteBtn, getString(R.string.university_website_url));
+        setButtonOnClickListener(studentCareBtn, getString(R.string.student_care_url));
+        setButtonOnClickListener(graduationBtn, getString(R.string.graduation_url));
+        setButtonOnClickListener(libraryBtn, getString(R.string.library_url));
     }
 
     private void findViews() {
         studentsWebBtn = (Button) findViewById(R.id.services_btn_1);
         eClassBtn = (Button) findViewById(R.id.services_btn_2);
         eudoxusBtn = (Button) findViewById(R.id.services_btn_3);
+
+        universityWebsiteBtn = (Button) findViewById(R.id.uni_services_btn_1);
+        studentCareBtn = (Button) findViewById(R.id.uni_services_btn_2);
+        graduationBtn = (Button) findViewById(R.id.uni_services_btn_3);
+        libraryBtn = (Button) findViewById(R.id.uni_services_btn_4);
+    }
+
+    private void setButtonOnClickListener(Button btn, String url){
+        btn.setOnClickListener(view -> {
+            Intent intent = new Intent(this, WebActivity.class);
+            intent.putExtra("url", url);
+            startActivity(intent);
+        });
     }
 }
