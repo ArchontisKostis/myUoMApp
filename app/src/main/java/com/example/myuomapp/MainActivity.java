8bb439abf3141d8a52ddcdc85462424393425241
DESKTOP-EMSE65H\Archontis <arxontisk02@gmail.com>
2023-03-14 18:14:27 +0200

    Replace the fake login with department select

--- app/src/main/java/com/example/myuomapp/MainActivity.java
+++ app/src/main/java/com/example/myuomapp/MainActivity.java
@@ -5,23 +5,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-    Button loginButton;
+    Spinner departmentSelectSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        loginButton = (Button) findViewById(R.id.login_btn);
-//        loginButton.setOnClickListener(view -> {
-//            Log.w("INFO", "Fake production sign in (MainActivity)");
-//            Intent intent = new Intent(this, HomeActivity.class);
-//            Log.i("INFO", "Start HomeActivity");
-//            startActivity(intent);
-//        });
+        departmentSelectSpinner = (Spinner) findViewById(R.id.department_select_spinner);
+        departmentSelectSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedDepartment = departmentSelectSpinner
+                        .getSelectedItem().toString();
+                if(!selectedDepartment.equals("--- ΕΠΙΛΕΞΤΕ ΤΜΗΜΑ ---")) {
+                    Log.d("MyUoM App", "Selected " + selectedDepartment + " Department.");
+
+                    Log.d("MyUoM App", "Create and Start HomeActivity.");
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    intent.putExtra("department", selectedDepartment);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                // Do nothing
+            }
+        });
 
         Log.i("INFO", "MainActivity created");
     }
